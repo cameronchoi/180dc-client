d5688263b8b849df4572a6537325cea7ee9c1d07
Cameron Choi <cameron.choi99@gmail.com>
2020-10-06 11:37:10 +1100

    add default case to authcontext to remove lint error

--- src/contexts/AuthContext.js
+++ src/contexts/AuthContext.js
@@ -29,6 +29,7 @@ function reducer(prevState, action) {
         userToken: null,
         position: null,
       };
+    default:
   }
 }
 
@@ -39,51 +40,6 @@ export function AuthProvider({ children }) {
     userToken: null,
     position: null,
   });
-
-  //   useEffect(() => {
-  //     // Fetch the token from storage then navigate to our appropriate place
-  //     const bootstrapAsync = async () => {
-  //       let userToken
-
-  //       try {
-  //         userToken = await AsyncStorage.getItem('userToken')
-  //       } catch (e) {
-  //         // Restoring token failed
-  //         console.log('THE TOKEN COULD NOT BE RETRIEVED')
-  //       }
-
-  //       // After restoring token, we may need to validate it in production apps
-
-  //       // This will switch to the App screen or Auth screen and this loading
-  //       // screen will be unmounted and thrown away.
-  //       dispatch({ type: 'RESTORE_TOKEN', token: userToken })
-  //     }
-
-  //     bootstrapAsync()
-  //   }, [])
-
-  //   const authContext = useMemo(
-  //     () => ({
-  //       signIn: async data => {
-  //         // In a production app, we need to send some data (usually username, password) to server and get a token
-  //         // We will also need to handle errors if sign in failed
-  //         // After getting token, we need to persist the token using `AsyncStorage`
-  //         // In the example, we'll use a dummy token
-
-  //         dispatch({ type: 'SIGN_IN', token: 'dummy-auth-token' })
-  //       },
-  //       signOut: () => dispatch({ type: 'SIGN_OUT' }),
-  //       signUp: async data => {
-  //         // In a production app, we need to send user data to server and get a token
-  //         // We will also need to handle errors if sign up failed
-  //         // After getting token, we need to persist the token using `AsyncStorage`
-  //         // In the example, we'll use a dummy token
-
-  //         dispatch({ type: 'SIGN_IN', token: 'dummy-auth-token' })
-  //       }
-  //     }),
-  //     []
-  //   )
 
   return (
     <AuthContext.Provider value={[state, dispatch]}>
