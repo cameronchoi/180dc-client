e64c2c2e706c7439db5d4a83a78f9d048349402b
Allen Hui <allenhui1220@outlook.com>
2021-02-23 21:25:33 +1100

    Fixed admin page bug

--- src/components/AdminRoute.js
+++ src/components/AdminRoute.js
@@ -10,7 +10,7 @@ const Admin = ({ component: Component, ...rest }) => {
     <Route
       {...rest}
       render={(props) =>
-        admin ? (
+        admin === "true" ? (
           <Component {...props} />
         ) : (
           <Redirect
