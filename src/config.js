90c1c1751f9ffaec470e218e6f9414058b6302b1
Allen Hui <allenhui1220@outlook.com>
2021-02-28 22:37:48 +1100

    http to https

--- src/config.js
+++ src/config.js
@@ -3,6 +3,6 @@ var base_url = 'admin.180dcusyd.org'
 
 export const proj_confs = {
     base: base_url,
-    root: `http://${base_url}`,
+    root: `https://${base_url}`,
     ws: `ws://${base_url}`,
 };
