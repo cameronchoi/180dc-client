65f90ecd3c2bfed4e9aae5839c0d61b46e4a3af4
Allen Hui <allenhui1220@outlook.com>
2021-03-01 21:22:21 +1100

    Secured web socket

--- src/config.js
+++ src/config.js
@@ -4,5 +4,5 @@ var base_url = 'admin.180dcusyd.org'
 export const proj_confs = {
     base: base_url,
     root: `https://${base_url}`,
-    ws: `ws://${base_url}`,
+    ws: `wss://${base_url}`,
 };
